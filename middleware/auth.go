288b5136ff8db8baed4fa56557a004c2d15d236d
ilhum.sa <ilhum.sa@inet.co.th>
2025-03-12 00:33:08 +0700

    Remove demo API key bypass from authentication middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -12,10 +12,6 @@ func Auth() fiber.Handler {
 
 		// For demo purposes, we'll just check for a mock API key
 		apiKey := c.Get("X-API-Key")
-		// if apiKey == "" {
-		// 	// If no API key provided, let it pass (for demo/testing)
-		// 	return c.Next()
-		// }
 
 		// For demo, we'll accept any API key that's at least 10 characters
 		if len(apiKey) < 10 {
